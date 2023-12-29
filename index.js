50de8ddbeaf629c3bca2d47fabf3479fcdd86830
Jivak Sute <jivakysute@gmail.com>
2023-12-29 13:44:32 +0530

    cors update

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const { UserModel } = require("./models/userModel");
 const app = express();
 const cors = require("cors");
 require("dotenv").config();
-app.use(cors());
+// app.use(cors());
 app.use(express.json());
 app.use(cors({ origin: "https://fitnesshub-ivory.vercel.app" }));
 
